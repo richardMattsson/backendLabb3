f2bc4b00921446fe6f619bb04fcdb1ac97d2966d
richardMattsson <mattsson.richard@gmail.com>
2025-05-06 11:38:57 +0200

    repare fetchUsersbyRole

--- frontend/src/views/registerUserView.vue
+++ frontend/src/views/registerUserView.vue
@@ -24,17 +24,13 @@ const userName = ref(''),
     <BRow>
       <BCol cols="6">
         <BForm id="registerForm">
-          <BFormGroup
-            id="input-group-3"
-            label="Användarnamn:"
-            label-for="input-3"
-          >
+          <BFormGroup id="input-group-3" label="Email:" label-for="input-3">
             <BFormInput
               id="input-3"
               type="text"
               class="mb-2"
               v-model="userName"
-              placeholder="Användarnamn"
+              placeholder="Email"
               required
             />
           </BFormGroup>
@@ -71,77 +67,8 @@ const userName = ref(''),
             </BFormValidFeedback>
           </BFormGroup>
 
-          <BFormGroup id="input-group-6" label="Förnamn:" label-for="input-6">
-            <BFormInput
-              id="input-6"
-              type="text"
-              class="mb-2"
-              v-model="firstName"
-              placeholder="Förnamn"
-              required
-            />
-          </BFormGroup>
-
-          <BFormGroup id="input-group-7" label="Efternamn:" label-for="input-7">
-            <BFormInput
-              id="input-7"
-              type="text"
-              class="mb-2"
-              v-model="lastName"
-              placeholder="Efternamn"
-              required
-            />
-          </BFormGroup>
-
-          <BFormGroup
-            id="input-group-8"
-            label="Användarnamn:"
-            label-for="input-8"
-          >
-            <BFormInput
-              id="input-8"
-              type="text"
-              class="mb-2"
-              v-model="phone"
-              placeholder="Telefon"
-              required
-            />
-          </BFormGroup>
-
-          <BFormGroup id="input-group-9" label="Email:" label-for="input-9">
-            <BFormInput
-              id="input-9"
-              type="text"
-              class="mb-2"
-              v-model="email"
-              placeholder="Email"
-              required
-            />
-          </BFormGroup>
-
-          <BFormGroup id="input-group-10" label="Stad:" label-for="input-10">
-            <BFormInput
-              id="input-10"
-              type="text"
-              class="mb-2"
-              v-model="city"
-              placeholder="Stad"
-              required
-            />
-          </BFormGroup>
-
           <BButton
-            @click="
-              taskStore.register(
-                userName,
-                password,
-                firstName,
-                lastName,
-                phone,
-                email,
-                city
-              )
-            "
+            @click="taskStore.register(userName, password)"
             class="mt-4"
             variant="success"
             >Skapa användare</BButton
