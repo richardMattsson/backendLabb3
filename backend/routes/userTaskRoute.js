bacba9212a15f51be06c31a50fb0b46617fe660a
richardMattsson <mattsson.richard@gmail.com>
2025-05-07 15:49:59 +0200

    add confirm doer endpoint in backend

--- backend/routes/userTaskRoute.js
+++ backend/routes/userTaskRoute.js
@@ -5,6 +5,8 @@ const userTaskController = require('../controllers/userTaskController');
 router.get('/api/user-tasks', userTaskController.getUserTasks);
 router.get('/api/user-tasks/:userId', userTaskController.getUserTask);
 router.post('/api/user-tasks', userTaskController.createUserTask);
+router.post('/api/confirm-doer', userTaskController.confirmDoer);
+
 router.put('/api/user-tasks', userTaskController.updateUserTask);
 router.delete('/api/user-tasks/:userId', userTaskController.deleteUserTask);
 
