1b7bcf019d7fc96d7321d6c842d36810892fe20f
Louise <nensen-lagnefors@hotmail.com>
2025-05-08 11:22:09 +0200

    Add userStore and change fetching data from taskStore to userStore

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,12 +1,16 @@
 <script setup>
-import { onMounted } from "vue"
-import { useTaskStore } from "@/stores/taskStore"
+import { ref, onMounted } from "vue"
+import { useUserStore } from "@/stores/userStore"
 
-const taskStore = useTaskStore()
+
+const userStore = useUserStore()
 const userId = 2
+const form = ref({})
+
+const isEditing = ref(false)
 
 onMounted(async () => {
-    await taskStore.fetchUserTasksbyRole(userId)
+    await userStore.fetchUserTasksByRole(userId)
 })
 </script>
 <template>
@@ -17,30 +21,30 @@ onMounted(async () => {
                 <h4>Min Profil</h4>
                 <b-form>
                     <b-row class="mb-2">
-                        <b-col cols="6"> <b-form-group label="Förnamn">
-                                <b-form-input></b-form-input>
+                        <b-col cols="6"> <b-form-group label="Förnamn" label-for="firstName">
+                                <b-form-input v-model="form.firstName" :disabled="!isEditing"></b-form-input>
                             </b-form-group>
                         </b-col>
-                        <b-col cols="6"> <b-form-group label="Efternamn">
-                                <b-form-input></b-form-input>
+                        <b-col cols="6"> <b-form-group label="Efternamn" label-for="lastName">
+                                <b-form-input v-model="form.lastName" :disabled="!isEditing"></b-form-input>
                             </b-form-group>
                         </b-col>
                     </b-row>
-
+                    <!--E-mail ska inte kunna ändras, då den är kopplad till inloggning -->
                     <b-row class="mb-2">
-                        <b-form-group label="E-post">
-                            <b-form-input></b-form-input>
+                        <b-form-group label="E-post" label-for="email">
+                            <b-form-input v-model="form.email" :disabled="!isEditing"></b-form-input>
                         </b-form-group>
                     </b-row>
-
                     <b-row class="mb-4">
                         <b-col cols="6">
-                            <b-form-group label="Telefonnummer">
-                                <b-form-input></b-form-input>
+                            <b-form-group label="Telefonnummer" label-for="phone">
+                                <b-form-input v-model="form.phone" :disabled="!isEditing"></b-form-input>
                             </b-form-group>
                         </b-col>
-                        <b-col cols="6"> <b-form-group label="Stad">
-                                <b-form-input></b-form-input>
+                        <b-col cols="6">
+                            <b-form-group label="Stad" label-for="city">
+                                <b-form-input v-model="form.city" :disabled="!isEditing"></b-form-input>
                             </b-form-group>
                         </b-col>
                     </b-row>
@@ -53,13 +57,24 @@ onMounted(async () => {
                 </b-form>
             </b-col>
         </b-row>
+
+        <!-- Loading and Error Handling -->
+        <b-row>
+            <b-col cols="12">
+                <div v-if="userStore.loading">Laddar uppdrag...</div>
+                <div v-else-if="userStore.error" class="text-danger">
+                    Fel: {{ userStore.error }}
+                </div>
+            </b-col>
+        </b-row>
+
         <!-- Performer Task List -->
         <b-row>
             <b-col md="6">
                 <h4 class="mt-3">Uppdrag att genomföra</h4>
                 <b-list-group>
-                    <template v-if="taskStore.performerTasks.length">
-                        <b-list-group-item v-for="task in taskStore.performerTasks" :key="task.taskId">
+                    <template v-if="userStore.performerTasks.length">
+                        <b-list-group-item v-for="task in userStore.performerTasks" :key="task.taskId">
                             <b-card class="mb-2">
                                 <h5 class="mb-1">{{ task.title }}</h5>
                                 <p class="mb-0">
@@ -81,8 +96,8 @@ onMounted(async () => {
             <b-col md="6">
                 <h4 class="mt-3">Beställda uppdrag</h4>
                 <b-list-group>
-                    <template v-if="taskStore.clientTasks.length">
-                        <b-list-group-item v-for="task in taskStore.clientTasks" :key="task.taskId">
+                    <template v-if="userStore.clientTasks.length">
+                        <b-list-group-item v-for="task in userStore.clientTasks" :key="task.taskId">
                             <b-card class="mb-2">
                                 <h5 class="mb-1">{{ task.title }}</h5>
                                 <p class="mb-0"><strong>Datum: </strong>{{ task.date }}</p>
@@ -94,16 +109,6 @@ onMounted(async () => {
                         <p class="text-muted ps-3">Du har inga beställda uppdrag</p>
                     </template>
                 </b-list-group>
-            </b-col>
-        </b-row>
-
-        <!-- Loading and Error Handling -->
-        <b-row>
-            <b-col cols="12">
-                <div v-if="taskStore.loading">Laddar uppdrag...</div>
-                <div v-else-if="taskStore.error" class="text-danger">
-                    Fel: {{ taskStore.error }}
-                </div>
             </b-col>
         </b-row>
     </b-container>
